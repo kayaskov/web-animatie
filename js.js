bd8cc72df5a89b1e7c84fefcc0ff09d38fdb3aca
kayaskov <45425078+kayaskov@users.noreply.github.com>
2020-05-25 08:12:24 +0200

    javascript click

--- js.js
+++ js.js
@@ -1,6 +1,12 @@
 var cirkel = document.querySelector('#Cirkel_4_');
 var target = document.querySelector('body');
 var ovaal = document.querySelector('#Ovaal_3_');
+var drum = document.querySelector('#Drum_1_');
+var stip1 = document.querySelector('#stip_2_1_');
+var stip2 = document.querySelector('#stip_1_1_');
+var streep1 = document.querySelector('#streep_2_1_');
+var streep2 = document.querySelector('#streep_1_2_');
+var streep3 = document.querySelector('#streep_3_2_');
 
 cirkel.addEventListener('click', function () {
     target.classList.toggle('effect');
@@ -9,6 +15,31 @@ cirkel.addEventListener('click', function () {
 ovaal.addEventListener('click', function () {
     target.classList.toggle('effect2');
 })
+
+drum.addEventListener('click', function () {
+    target.classList.toggle('effect3');
+})
+
+stip1.addEventListener('click', function () {
+    target.classList.toggle('effect4');
+})
+
+stip2.addEventListener('click', function () {
+    target.classList.toggle('effect4');
+})
+
+streep1.addEventListener('click', function () {
+    target.classList.toggle('effect5');
+})
+
+streep2.addEventListener('click', function () {
+    target.classList.toggle('effect5');
+})
+
+streep3.addEventListener('click', function () {
+    target.classList.toggle('effect5');
+})
+
 
 window.addEventListener('keydown', toggle)
 
