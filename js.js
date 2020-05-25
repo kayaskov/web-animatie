d3d99a68d6d1978820da0331b1d8e8210c75c785
kayaskov <45425078+kayaskov@users.noreply.github.com>
2020-05-25 07:47:47 +0200

    background effect on keydown

--- js.js
+++ js.js
@@ -1,9 +1,21 @@
 var cirkel = document.querySelector('#Cirkel_4_');
 var target = document.querySelector('body');
+var ovaal = document.querySelector('#Ovaal_3_');
 
 cirkel.addEventListener('click', function () {
     target.classList.toggle('effect');
 })
+
+ovaal.addEventListener('click', function () {
+    target.classList.toggle('effect2');
+})
+
+window.addEventListener('keydown', toggle)
+
+function toggle(event) {
+    console.log(event.keyCode);
+    target.classList.toggle('key');
+}
 //
 //function toggle() {
 //   drum.classList.toggle('drumkleur')
