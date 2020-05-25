4ae73d5f60f876d82526f772df92aeec41f057aa
kayaskov <45425078+kayaskov@users.noreply.github.com>
2020-05-25 07:23:20 +0200

    java

---
+++ js.js
@@ -0,0 +1,11 @@
+var cirkel = document.querySelector('#Cirkel_4_');
+var target = document.querySelector('body');
+
+cirkel.addEventListener('click', function () {
+    target.classList.toggle('effect');
+})
+//
+//function toggle() {
+//   drum.classList.toggle('drumkleur')
+//}
+//
